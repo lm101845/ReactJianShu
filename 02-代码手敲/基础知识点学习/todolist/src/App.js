ada20000f49b6d7cb284e2e51802564260d9cc10
lm101845 <1018457683@qq.com>
2021-09-10 17:50:33 +0800

    Auto commit.

---
+++ 02-代码手敲/基础知识点学习/todolist/src/App.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: liming
+ * @Date: 2021-09-10 17:28:07
+ * @LastEditTime: 2021-09-10 17:44:40
+ * @FilePath: \ReactJianShu\02-代码手敲\基础知识点学习\todolist\src\App.js
+ */
+
+function App() {
+  return (
+    <div>
+          <h2>你好</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-代码手敲/基础知识点学习/todolist/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: liming
+ * @Date: 2021-09-10 17:28:07
+ * @LastEditTime: 2021-09-10 17:44:54
+ * @FilePath: \ReactJianShu\02-代码手敲\基础知识点学习\todolist\src\index.js
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
